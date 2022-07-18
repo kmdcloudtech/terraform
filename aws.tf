b8b9f9eec506df290fd4252ad205a342148f4c66
kmdcloudtech <93834713+kmdcloudtech@users.noreply.github.com>
2022-07-18 18:35:53 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-08df646e18b182346"
+  ami           = "ami-068257025f72f470d"
   instance_type = "t2.micro"
   count         = 1    #
   subnet_id = "subnet-0d47c7cc17ec8f644"
