023a09735b654c63d99506448a26a495f5370b0f
kmdcloudtech <93834713+kmdcloudtech@users.noreply.github.com>
2022-07-18 18:25:48 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,24 +1,11 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu-server" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "web" {
+  ami           = "ami-08df646e18b182346"
   instance_type = "t2.micro"
+  count         = 1    #
+  subnet_id = "subnet-0d47c7cc17ec8f644"
 
   tags = {
-    Name = "ubuntu-server"
+    Name = "web"
   }
 }
+
